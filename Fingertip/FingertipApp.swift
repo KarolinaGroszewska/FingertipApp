55d4389650676b68ccaca08190dd7f51b4b4da95
KarolinaGroszewska <karolina.groszewska@gmail.com>
2022-12-25 16:39:03 -0600

    Initial Commit

---
+++ Fingertip/FingertipApp.swift
@@ -0,0 +1,17 @@
+//
+//  FingertipApp.swift
+//  Fingertip
+//
+//  Created by Kari on 12/25/22.
+//
+
+import SwiftUI
+
+@main
+struct FingertipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
