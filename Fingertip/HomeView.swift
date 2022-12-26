d88e06c4a13ed27ae941778d52d855de371473d5
KarolinaGroszewska <karolina.groszewska@gmail.com>
2022-12-25 19:44:03 -0600

    Begin workflow for redirect

---
+++ Fingertip/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Fingertip
+//
+//  Created by Kari on 12/25/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
